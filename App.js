d5c0c7ff249428d1b6660ac7b860ef8ec45afc12
Anton Begehr <a.begehr@gmail.com>
2020-02-11 14:41:52 +0100

    app MapView and renderRandomMarkers in initialRegion

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+
+const initialRegion = {
+  latitude: 37.72825,
+  longitude: -122.4324,
+  latitudeDelta: 0.25,
+  longitudeDelta: 0.15
+};
+
+function renderRandomMarkers(n) {
+  const { latitude, longitude, latitudeDelta, longitudeDelta } = initialRegion;
+  return new Array(n).fill().map((x, i) => (
+    <Marker
+      key={i}
+      coordinate={{
+        latitude: latitude + (Math.random() - 0.5) * latitudeDelta,
+        longitude: longitude + (Math.random() - 0.5) * longitudeDelta
+      }}
+    />
+  ));
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView style={styles.map} initialRegion={initialRegion}>
+        {renderRandomMarkers(144)}
+      </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  map: {
+    width: "100%",
+    height: "100%"
+  }
 });
